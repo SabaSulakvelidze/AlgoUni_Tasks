a4e0034dc6e63e62c3edcde342478e1321b0aa88
Saba <sabasulakveidze@gmail.com>
2025-12-13 13:51:18 +0400

    feat: task 10 done

---
+++ IRaceVehicle.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoUni_Tasks
+{
+    internal interface IRaceVehicle
+    {
+        void ActivateTurbo();
+    }
+}
