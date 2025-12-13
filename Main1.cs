f60b387e2462660f3d22e2e8a81c42951515ea20
Saba <sabasulakveidze@gmail.com>
2025-12-13 14:14:23 +0400

    feat: done

---
+++ Main1.cs
@@ -0,0 +1,12 @@
+﻿internal class ProgramBase
+{
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("This is Main #1");
+
+    }
+}
---
+++ Main2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoUni_Tasks
+{
+    internal class Main2
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("This is Main #2");
+
+        }
+    }
+}
