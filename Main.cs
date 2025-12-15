a201e64c756e02e8e3f1bca872a54bc281b01cc8
Saba <sabasulakveidze@gmail.com>
2025-12-16 00:25:08 +0400

    feat: task 12 done

--- Main.cs
+++ Main.cs
@@ -44,7 +44,7 @@ internal class Program
         List<int> intArr = new List<int> { 1, 1, 2, 2, 3, 3, 4, 5, 6 };
         RemoveDuplicates<int>(intArr).ForEach(Console.WriteLine);
         List<string> strArr = new List<string> { "red", "blue", "red", "green", "green", "yellow" };
-        RemoveDuplicates<string>(strArr).ForEach(Console.WriteLine);*/
+        RemoveDuplicates<string>(strArr).ForEach(Console.WriteLine);
 
         //შექმენით List<int>, რომელიც შეიცავს რიცხვებს 1 - დან 10 - მდე.დაამატეთ ამ სიაში რიცხვი 11 და გამოიტანეთ სიის
         //ყველა ელემენტი.
@@ -97,39 +97,143 @@ internal class Program
         //მეტია 15 - ზე.
         List<int> list3 = Enumerable.Range(10, 20).ToList();
         list3.RemoveAll(n => n > 15);
-        list3.ForEach(Console.WriteLine);
+        list3.ForEach(Console.WriteLine);*/
 
         //შექმენით Dictionary<int, string>, რომელიც შეიცავს 5 წყვილს, სადაც key არის რიცხვი და value არის რიცხვის
         //დასახელება(მაგ.: 1-> "One").დაამატეთ ახალი წყვილი და გამოიტანეთ ყველა წყვილი.
+        Dictionary<int, string> dictionary = new() {
+            { 1, "one" },
+            { 2, "two" },
+            { 3, "three" },
+            { 4, "four" },
+            { 5, "five" }
+        };
+        dictionary.Add(7,"seven");
+        foreach (var item in dictionary)
+            Console.WriteLine($"key: {item.Key} | value: {item.Value}");
 
         //შექმენით Dictionary<string, int>, რომელიც შეიცავს 5 სტუდენტის სახელს და მათ შეფასებებს.იპოვეთ სტუდენტი,
         //რომლის შეფასება ყველაზე მაღალია.
+        Dictionary<string, int> grades = new() {
+            {"john",33 },
+            {"mary",67 },
+            {"george",97 },
+            {"luke",57 },
+            {"anna",85 }
+        };
+        int maxGrade = grades.Select(i => i.Value).ToList().Max();
+        Console.WriteLine(grades.Where(i=>i.Value==maxGrade).First().Key);
 
         //შექმენით Dictionary<string, string>, დაამატეთ 3 ქვეყნის სახელი და მათი დედაქალაქები.შეცვალეთ ერთი ქვეყნის
         //დედაქალაქი და გამოიტანეთ განახლებული სია.
+        Dictionary<string, string> countries = new() {
+            { "Georgia", "Kutaisi" },
+            { "France", "Paris" },
+            { "India", "New Dehli" },
+        };
+        countries["Georgia"] = "Tbilisi";
+        foreach (var item in countries)
+            Console.WriteLine($"{item.Key} : {item.Value}");
 
         //შექმენით Dictionary<int, string>, სადაც შეინახავთ მანქანის ნომრებს და მძღოლის სახელს.წაშალეთ ერთი ელემენტი
         //და გამოიტანეთ დარჩენილი მონაცემები.
+        Dictionary<int, string> drivers = new()
+        {
+            { 471, "Giorgi" },
+            { 123, "Tamar" },
+            { 555, "Lasha" },
+            { 117, "Mariami" }
+        };
+        drivers.Remove(555);
+        foreach (var item in drivers)
+            Console.WriteLine($"{item.Key} : {item.Value}");
 
         //შექმენით Dictionary<string, int>, რომელიც შეიცავს 5 სტუდენტის სახელს და მათი ასაკები.იპოვეთ სტუდენტი,
         //რომლის ასაკი არის 18 წელზე ნაკლები.
+        Dictionary<string, int> students = new()
+        {
+            { "Luka", 21 },
+            { "Ana", 17 },
+            { "Davit", 22 },
+            { "Eka", 20 },
+            { "Sandro", 15 }
+        };
+
+        Dictionary<string, int> ofAgeStudents = students.Where(s => s.Value < 18).ToDictionary<string, int>();
+        foreach (var item in ofAgeStudents)
+            Console.WriteLine($"{item.Key} : {item.Value}");
 
         //შექმენით Dictionary<int, string> და დაამატეთ 5 ქალაქის დასახელება მათი ID-ების მიხედვით.შეამოწმეთ, შეიცავს
         //თუ არა სია ქალაქის სახელ "Tbilisi" - ს და შედეგი გამოიტანეთ.
+        Dictionary<int, string> cities = new()
+        {
+            { 1, "Tbilisi" },
+            { 2, "Kutaisi" },
+            { 3, "Batumi" },
+            { 4, "Rustavi" },
+            { 5, "Gori" }
+        };
+        Console.WriteLine(cities.ContainsValue("Tbilisi"));
 
         //შექმენით Dictionary<string, string>, რომელიც შეიცავს 5 წიგნის ავტორს და მათ წიგნებს.დაამატეთ ახალი ავტორი
         //და წიგნი და გამოიტანეთ განახლებული სია.
+        Dictionary<string, string> books = new()
+        {
+            { "Jane Austen", "Pride and Prejudice" },
+            { "George Orwell", "1984" },
+            { "F. Scott Fitzgerald", "The Great Gatsby" },
+            { "Virginia Woolf", "Mrs. Dalloway" },
+            { "J.R.R. Tolkien", "The Hobbit" }
+        };
+        books.Add("Ernest Hemingway", "The Old Man and the Sea");
+        foreach (var item in books)
+            Console.WriteLine($"{item.Key} : {item.Value}");
 
         //შექმენით Dictionary<int, string> და შეინახეთ თვეების დასახელებები მათი რიგითი ნომრების მიხედვით(1->January,
         //2->February და ა.შ.).დაამატეთ ახალი თვე და გამოიტანეთ სია.
+        Dictionary<int, string> months = new()
+        {
+            { 1, "January" },
+            { 2, "February" },
+            { 3, "March" },
+            { 4, "April" },
+            { 5, "May" },
+            { 6, "June" },
+            { 7, "July" },
+            { 8, "August" },
+            { 9, "September" },
+            { 10, "October" },
+            { 11, "November" },
+            { 12, "December" }
+        };
+        months.Add(13, "Mecamete");
+        foreach (var item in months)
+            Console.WriteLine($"{item.Key} : {item.Value}");
 
         //შექმენით Dictionary<int, string>, რომელიც შეიცავს 5 სხვადასხვა პროდუქტს მათი ID-ების მიხედვით.იპოვეთ პროდუქტი,
         //რომლის ID უდრის 3 - ს და გამოიტანეთ მისი სახელი.
+        Dictionary<int, string> products = new()
+        {
+            { 1, "Laptop" },
+            { 2, "Mouse" },
+            { 3, "Keyboard" },
+            { 4, "Monitor" },
+            { 5, "Webcam" }
+        };
+        Console.WriteLine(products[3]);
 
         //შექმენით Dictionary<string, int> და დაამატეთ 5 სტუდენტის მონაცემები(სახელი -> შეფასება). შეცვალეთ ერთ-ერთი
         //სტუდენტის შეფასება და გამოიტანეთ განახლებული მონაცემები.
-
-
+        Dictionary<string, int> stidentsGrades = new() {
+            {"john",33 },
+            {"mary",67 },
+            {"george",97 },
+            {"luka",57 },
+            {"anna",85 }
+        };
+        stidentsGrades["luka"] = 89;
+        foreach (var item in stidentsGrades)
+            Console.WriteLine($"{item.Key} : {item.Value}");
     }
 
     public static T[] RemoveElement<T>(T[] arr, int index)
